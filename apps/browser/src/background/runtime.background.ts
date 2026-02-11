2fa32854b22b67d96cf15fa5e78eab97827d3b95
Jonathan Prusik <jprusik@classynemesis.com>
2026-02-11 18:25:41 -0500

    use targeting rules to qualify relevant fields

--- apps/browser/src/background/runtime.background.ts
+++ apps/browser/src/background/runtime.background.ts
@@ -87,6 +87,7 @@ export default class RuntimeBackground {
         BiometricsCommands.GetBiometricsStatusForUser,
         BiometricsCommands.CanEnableBiometricUnlock,
         "getUserPremiumStatus",
+        "getUrlAutofillTargetingRules",
       ];
 
       if (messagesWithResponse.includes(msg.command)) {
@@ -219,6 +220,9 @@ export default class RuntimeBackground {
           this.billingAccountProfileStateService.hasPremiumFromAnySource$(activeUserId),
         );
         return result;
+      }
+      case "getUrlAutofillTargetingRules": {
+        return await this.main.domainSettingsService.getTargetingRulesForUrl(sender.tab?.url);
       }
     }
   }
